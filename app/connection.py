a3b3fbe9b7ce395d0886130f563f77b0fa6e2e91
Naftali Horviz <n0548498093@gmail.com>
2026-02-05 11:32:53 +0200

    fix connection

--- app/connection.py
+++ app/connection.py
@@ -1,7 +1,20 @@
 from pymongo import MongoClient
+import os
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client['week_16']
+host = os.getenv("MONGO_HOST")
+port = int(os.getenv("MONGO_PORT"))
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")
+db_name = os.getenv("MONGO_DB")
+auth_source = os.getenv("MONGO_AUTH_SOURCE")
+
 
 def get_col():
+    client = MongoClient(
+        host=host,
+        port=port,
+        username=username,
+        password=password,
+    )
+    db = client[db_name]
     return db['employees']
